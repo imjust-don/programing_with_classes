1032fc612205d6e0d159edab5ada482bc651c5fc
Brandon Harbican <blharbican@outlook.com>
2023-09-18 09:57:13 -0600

    qwerqwer

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        int number = 0;
+        List<int> user_number;
+        user_number = new List<int>();
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        do
+        {
+            Console.Write("Please enter a number: ");
+            string inpt = Console.ReadLine();
+            number = int.Parse(inpt);
+            user_number.Add(number);
+        }while (number != 0);
     }
 }
