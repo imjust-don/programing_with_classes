547321283def7593e35a48f16d40fc0f0f3fddf7
Brandon Harbican <blharbican@outlook.com>
2023-09-18 09:44:23 -0600

    rtwertwert

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.WriteLine("Welcome to the number guesser");
+        Console.WriteLine("Im thinking a number guess what I'm thinking 1-10");
+
+        string guess = "0";
+
+        do
+        {
+            Console.Write("What is your Guess? ");
+            guess = Console.ReadLine();
+            
+        }while (guess != "7");
+
+        Console.WriteLine($"Congrats! The number was {guess}!");
     }
 }
