8a819bf98627b95f90cd133e379421164a1ce3d1
Brandon Harbican <blharbican@outlook.com>
2023-09-18 12:04:05 -0600

    asfdasdf

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -5,6 +5,9 @@ class Program
     static void Main(string[] args)
     {
         int number = 0;
+        int x = 0;
+        float average = 0;
+        int count = 0;
         List<int> user_number;
         user_number = new List<int>();
         Console.WriteLine("Enter a list of numbers, type 0 when finished.");
@@ -15,5 +18,15 @@ class Program
             number = int.Parse(inpt);
             user_number.Add(number);
         }while (number != 0);
+
+        foreach (int i in user_number)
+        {
+            x = x + i;
+            count = count + 1;
+
+        }
+
+        Console.WriteLine($"The sume of all your numbers is: {x}");
+        Console.WriteLine($"The average is: {x/(count-1)}");
     }
 }
