1838f27fcffd60325cdccc4f996f54caf63c0c27
Brandon Harbican <blharbican@outlook.com>
2023-09-27 10:00:16 -0600

    kjhlkjh

---
+++ in class/tournament/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Player ronaldo = new Player("Rolnaldo", 12);
+ronaldo.Display();
+
+Player Jack = new Player("Jack", 12);
+Jack.Display();
+
+Team Dragons = new Team("Dragons");
+Dragons.AddPLayer(ronaldo);
+Dragons.AddPLayer(Jack);
+
+Dragons.DisplayRoster();
---
+++ in class/tournament/match.cs
@@ -0,0 +1,16 @@
+public class Match
+{
+    private Team _team1;
+    private Team _team2;
+
+    public Match(Team team1, Team team2)
+    {
+        _team1 = team1
+        _team2 = team2
+    }
+
+    
+
+}
+
+
