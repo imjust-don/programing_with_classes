85cdc77dca91db8d493fd1fa6d3f245457dac765
Brandon Harbican <blharbican@outlook.com>
2023-12-05 12:13:58 -0700

    foundation2

--- final/Foundation2/Program.cs
+++ final/Foundation2/Program.cs
@@ -2,8 +2,31 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+     static void Main()
     {
-        Console.WriteLine("Hello Foundation2 World!");
+       
+        var usaAddress = new Address("123 Main St", "Anytown", "CA", "USA");
+        var nonUsaAddress = new Address("456 High St", "Othercity", "Province", "Canada");
+
+        
+        var usaCustomer = new Customer("John Doe", usaAddress);
+        var nonUsaCustomer = new Customer("Jane Smith", nonUsaAddress);
+
+        
+        var product1 = new Product("Laptop", 101, 800, 2);
+        var product2 = new Product("Mouse", 102, 20, 3);
+
+      
+        var order1 = new Order(new List<Product> { product1, product2 }, usaCustomer);
+        var order2 = new Order(new List<Product> { product2 }, nonUsaCustomer);
+
+        
+        Console.WriteLine(order1.GetPackingLabel());
+        Console.WriteLine(order1.GetShippingLabel());
+        Console.WriteLine($"Total Price: ${order1.CalculateTotalPrice():F2}\n");
+
+        Console.WriteLine(order2.GetPackingLabel());
+        Console.WriteLine(order2.GetShippingLabel());
+        Console.WriteLine($"Total Price: ${order2.CalculateTotalPrice():F2}\n");
     }
 }
---
+++ final/Foundation2/order.cs
@@ -0,0 +1,47 @@
+class Order
+{
+    private List<Product> products;
+    private Customer customer;
+
+    public Order(List<Product> products, Customer customer)
+    {
+        this.products = products;
+        this.customer = customer;
+    }
+
+    public decimal CalculateTotalPrice()
+    {
+        decimal totalPrice = 0;
+        foreach (var product in products)
+        {
+            totalPrice += product.GetTotalPrice();
+        }
+
+        // Add shipping cost based on customer location
+        if (customer.IsInUSA())
+        {
+            totalPrice += 5;
+        }
+        else
+        {
+            totalPrice += 35;
+        }
+
+        return totalPrice;
+    }
+
+    public string GetPackingLabel()
+    {
+        string packingLabel = "Packing Label:\n";
+        foreach (var product in products)
+        {
+            packingLabel += $"{product.GetProductInfo()}\n";
+        }
+        return packingLabel;
+    }
+
+    public string GetShippingLabel()
+    {
+        return $"Shipping Label:\nCustomer: {customer.GetName()}\nAddress: {customer.GetAddress().GetFullAddress()}";
+    }
+}
